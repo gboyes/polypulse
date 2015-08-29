700e8cea41d53a32c53ce39c508402e4c0c48fc8
Graham Boyes <gr.ed.boyes@gmail.com>
2015-08-29 12:33:34 -0400

    Small layout change, added animation to layer

--- polypulse/ViewController.swift
+++ polypulse/ViewController.swift
@@ -295,8 +295,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     //MARK: audio engine delegate
     func updatedRepresentativeBufferValue(val: UnsafeMutablePointer<Float>) {
         dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+            
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.indicatorView.backgroundColor = UIColor(red: CGFloat(val[0]), green: CGFloat(val[1]), blue: CGFloat(val[2]), alpha: 1.0)
+                
+                UIView.animateWithDuration(1.0/30.0, animations: { () -> Void in
+                    self.indicatorView.layer.backgroundColor = UIColor(red: CGFloat(val[0]), green: CGFloat(val[1]), blue: CGFloat(val[2]), alpha: 1.0).CGColor
+                })
+                
+//                self.indicatorView.layer.backgroundColor = UIColor(red: CGFloat(val[0]), green: CGFloat(val[1]), blue: CGFloat(val[2]), alpha: 1.0).CGColor
                 return
             })
             
